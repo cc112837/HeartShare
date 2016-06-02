157b5f5cfaf698543271f2098b63fc3b3e2f7ba2
cc112837@163.com <cc112837@163.com>
2016-06-02 11:14:14 +0800

    修改成底部tab   添加我的页面布局

--- app/src/main/java/activity/MainActivity.java
+++ app/src/main/java/activity/MainActivity.java
@@ -1,17 +1,10 @@
 package activity;
 
 import android.os.Bundle;
-import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
@@ -30,21 +23,21 @@ import util.ChangeFragmentHelper;
  * 创建人：吴聪聪
  * 邮箱:cc112837@163.com
  * 创建时间：2016/4/19 11:50
-*/
+ */
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
     private Fragment fragment;
     TextView tv_title;
     ImageView iv_add;
     OnShareClickListener shareListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        tv_title=((TextView) toolbar.findViewById(R.id.tv_title));
-        iv_add=(ImageView) toolbar.findViewById(R.id.iv_add);
+        FrameLayout toolbar = (FrameLayout) findViewById(R.id.toolbar);
+        tv_title = ((TextView) toolbar.findViewById(R.id.tv_title));
+        iv_add = (ImageView) toolbar.findViewById(R.id.iv_add);
         tv_title.setText("首页");
 
         Fragment fragment = new HomeFragment();
@@ -57,80 +50,21 @@ public class MainActivity extends AppCompatActivity
 
         initView();
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
+
+        super.onBackPressed();
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            tv_title.setText("相机");
-            iv_add.setVisibility(View.INVISIBLE);
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-            tv_title.setText("照片");
-            iv_add.setVisibility(View.INVISIBLE);
-
-        } else if (id == R.id.nav_slideshow) {
-            tv_title.setText("播放");
-            iv_add.setVisibility(View.INVISIBLE);
-        } else if (id == R.id.nav_manage) {
-            tv_title.setText("设置");
-            iv_add.setVisibility(View.INVISIBLE);
-        } else if (id == R.id.nav_share) {
-            tv_title.setText("分享");
-            iv_add.setVisibility(View.INVISIBLE);
-        } else if (id == R.id.nav_send) {
-            tv_title.setText("反馈");
-            iv_add.setVisibility(View.INVISIBLE);
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
     public interface OnShareClickListener {
-        public void onClicked();
+        void onClicked();
     }
+
     private void initView() {
 
         //盛放Fragment的容器
